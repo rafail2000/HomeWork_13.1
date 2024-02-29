8648dda2aa1f9848d5eeab4fad88f93336c4fdcb
intech <rafail.shiryaev.2015@mail.ru>
2024-02-29 13:55:12 +0300

    HomeWork_13.2

--- src/classes.py
+++ src/classes.py
@@ -67,23 +67,14 @@ class Product:
                 self.__price = new_price
             else:
                 print('Цена осталась прежней')
+        else:
+            self.__price = new_price
+
 
     def get_product_price(self):
         """Получение приватного атрибута price"""
         return self.price
 
-    # @classmethod
-    # def add_new_product(cls, name, description, price, quantity, list_of_products=None):
-    #     """Поиск товаров с похожими названиями"""
-    #     new_product = cls(name, description, price, quantity)
-    #     if list_of_products:
-    #         for product in list_of_products:
-    #             if new_product.name == product.name:
-    #                 if new_product.price > product.price:
-    #                     product.price = new_product.price
-    #                 product.quantity += new_product.quantity
-    #     else:
-    #         return cls(name, description, price, quantity)
 
     def __repr__(self):
         return f'Product({self.name}, {self.description}, {self.price}, {self.quantity})'
@@ -104,28 +95,11 @@ class Product:
                     if product.price < price:
                         product.price = price
                     # установив атрибуты у продукта - возвращаем его
-                    return print(product)
+                    return product
 
         # здесь мы окажемся в двух случаях: если не передан список продуктов, либо он был передан но в цикле не нашлось совпадения по имени - значит мы должны создать продукт и вернуть его
         new_product = cls(name, description, price, quantity)
-        return print(new_product)
-
-    # @classmethod
-    # def add_new_product(cls, product_data, list_of_products=None):
-    #     name = product_data['name']
-    #     description = product_data['description']
-    #     price = product_data['price']
-    #     quantity = product_data['quantity']
-    #     if list_of_products:
-    #         for product in list_of_products:
-    #             if product.name == name:
-    #                 product.quantity += quantity
-    #                 if product.price > price:
-    #                     product.price = price
-    #                 return product
-    #     else:
-    #         new_product = cls(name, description, price, quantity)
-    #         return new_product
+        return new_product
 
 
 # создадим словарь для будущего продукта
@@ -136,8 +110,8 @@ samsung_data = {
     "quantity": 5
 }
 # и создадим продукт, с тем же именем, но с другими данными.
-samsung_another_product = Product('Samsung Galaxy C23 Ultra', 'описание', 2000000.0, 15)
+samsung_another_product = Product('Samsung Galaxy C23 Ultra', 'описание', 200000.0, 15)
 list_of_products = [samsung_another_product]
 # и используем метод, отдавая как словарь, так и список продуктов (в нашем случае один продукт в списке).
 samsung_product = Product.add_new_product(samsung_data, list_of_products)
-
+print(samsung_product)
