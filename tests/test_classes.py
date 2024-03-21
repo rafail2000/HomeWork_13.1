856f5f540d6344486169f65633392ab8db14f672
intech <rafail.shiryaev.2015@mail.ru>
2024-03-21 18:42:11 +0300

    HomeWork_15.1

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,11 +1,42 @@
 import pytest
 
-from src.classes import Category, Product
+from src.classes import Category, Product, Smartphone, LawnGrass
+
+
+def test_sum_product_error():
+    with pytest.raises(TypeError):
+        Smartphone("Iphone",
+                   "Unique design",
+                   100_000,
+                   15,
+                   "3.8",
+                   "15",
+                   2048,
+                   "black") + LawnGrass("Green grass",
+                                        "Usual green grass",
+                                        100,
+                                        1000,
+                                        "China",
+                                        "two weeks",
+                                        "green")
 
 
 def test_sum_product():
-    with pytest.raises(TypeError):
-        class_product + class_smartfon
+    assert Smartphone("Iphone",
+                      "Unique design",
+                      100_000,
+                      15,
+                      "3.8",
+                      "15",
+                      2048,
+                      "black") + Smartphone("Iphone",
+                                            "Unique design",
+                                            100_000,
+                                            20,
+                                            "3.8",
+                                            "14",
+                                            2048,
+                                            "black") == 3500000
 
 # @pytest.fixture
 # def class_category():
