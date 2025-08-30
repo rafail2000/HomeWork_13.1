824108c77aac4ab8860eeda4a273ec9e60bdad11
rafail.shiryaev.2017 <rafail.shiryaev.2015@mail.ru>
2025-08-30 19:43:08 +0300

    HomeWork_13_2.

--- src/file_reader_csv_xlsx.py
+++ src/file_reader_csv_xlsx.py
@@ -1,10 +1,15 @@
 import json
 import pandas as pd
+def read_json_file(path):
+    """ Чтение JSON файла """
+    with open(path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        return data
 
 
 def read_csv_file(path):
     """ Чтение csv файла. """
-    df = pd.read_csv(path)
+    df = pd.read_csv(path, delimiter=';')
     return df.to_dict(orient='records')
 
 
@@ -15,7 +20,7 @@ def read_excel_file(path):
 
 
 if __name__ == "__main__":
+    json_path = "../data/operations.json"
     csv_path = "../data/transactions.csv"
     excel_path = "../data/transactions_excel.xlsx"
     print(read_csv_file(csv_path))
-    print(read_excel_file(excel_path))
--- src/main.py
+++ src/main.py
@@ -1,30 +1,113 @@
-from pprint import pprint
-
-from src.classes import Category, Product
-from src.utils import load_data
+from src.file_reader_csv_xlsx import read_json_file, read_csv_file, read_excel_file
 
 
-# def main():
-#     data = load_data()
-#     list_category = []
-#     for unit in data:
-#         list_product = [un for un in unit["products"]]
-#         category = Category(unit["name"], unit["description"], unit["products"])
-#         list_category.append(f'{category.get_name()}\n'
-#                              f'{category.get_description()}\n'
-#                              f'{category.add_goods()}\n\n'
-#                              )
-#         result = []
-#         for element in list_product:
-#             product = Product(element["name"], element["description"],
-#                               element["price"], element["quantity"])
-#             result.append(f'{product.get_product_name()}\n'
-#                           f'{product.get_product_description()}\n'
-#                           f'{product.get_product_price()}\n'
-#                           f'{product.get_product_quantity_in_stock()}\n\n'
-#                           )
-#     pprint(list_category)
-#
-#
-# if __name__ == '__main__':
-#     main()
+def main():
+    """ Основная функция для взаимодействия с пользователем """
+
+    type_file = "" # Выбор типа файла JSON, CSV или, XLSX.
+    type_operation = "" # Выбор типа операции EXECUTED, CANCELED или PENDING.
+    sort_on_time = False # Сортировать по времени или нет.
+    sort_up_or_down = False # Сортировать по возрастанию ил по убыванию.
+    only_rub = False # Выводить только рублёвые транзакции, да или нет.
+    sort_on_word = False # сортировать по слову, да или нет.
+
+    while True:
+        type_files = ("JSON", "CSV", "XLSX")
+
+        print("""Привет! Добро пожаловать в программу работы 
+с банковскими транзакциями. 
+Выберите необходимый пункт меню:
+1. Получить информацию о транзакциях из JSON-файла
+2. Получить информацию о транзакциях из CSV-файла
+3. Получить информацию о транзакциях из XLSX-файла\n""")
+        user_input = input("Введите номер необходимого вам пункта: ")
+        if user_input not in "123":
+            print("Выберите пункты из представленных!")
+            continue
+        type_file = type_files[int(user_input) - 1]
+        print(f"Для обработки выбран {type_file}-файл.")
+        break
+
+    while True:
+        type_operations = ("executed", "canceled", "pending")
+
+        print("""Введите статус, по которому необходимо выполнить фильтрацию. 
+Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING""")
+        user_input = input("Введите статус: ").lower()
+        if user_input not in type_operations:
+            print(f"Статус операции {user_input} недоступен")
+            continue
+        type_operation = user_input.upper()
+        print(f"Операции отфильтрованы по статусу {type_operation}")
+        break
+
+    json_path = "../data/operations.json"
+    csv_path = "../data/transactions.csv"
+    excel_path = "../data/transactions_excel.xlsx"
+
+    if type_file == "JSON":
+        data = read_json_file(json_path)
+    elif type_file == "CSV":
+        data =  read_csv_file(csv_path)
+    else:
+        data = read_excel_file(excel_path)
+
+    filtered_data = [i for i in data if i.get('state', False) == type_operation]
+
+    while True:
+        yes_or_not = ("да", "нет")
+        up_or_down = ("по возрастанию", "по убыванию")
+        print("Отсортировать операции по дате Да/Нет?")
+        user_input = input("Введите да или нет: ").lower()
+        if user_input not in yes_or_not:
+            print("Необходимо выбрать, да или нет!")
+            continue
+        sort_on_time = True if user_input == "да" else False
+        if sort_on_time:
+            print("Отсортировать по возрастанию или по убыванию?")
+            user_input = input("Введите по возрастанию или по убыванию: ").lower()
+            if user_input not in up_or_down:
+                print("Необходимо выбрать, по возрастанию или убыванию!")
+                continue
+            sort_up_or_down = True if user_input == "по возрастанию" else False
+
+        print("Выводить только рублевые транзакции Да/Нет?")
+        user_input = input("Введите да или нет: ").lower()
+        if user_input not in yes_or_not:
+            print("Необходимо выбрать, да или нет!")
+            continue
+        only_rub = True if user_input == "да" else False
+
+        print("Отфильтровать список транзакций по определенному слову в описании да или нет?")
+        user_input = input("Введите да или нет: ").lower()
+        if user_input not in yes_or_not:
+            print("Необходимо выбрать, да или нет!")
+            continue
+        sort_on_word = True if user_input == "да" else False
+        break
+
+    if sort_on_time:
+        if sort_up_or_down:
+            filtered_data.sort(key=lambda x: x["date"], reverse=False)
+        else:
+            filtered_data.sort(key=lambda x: x["date"], reverse=True)
+
+    if only_rub:
+        filtered_data = [i for i in filtered_data if i.get("currency_code").lower() == "rub"]
+
+    if sort_on_word: # Так или нет???????
+        filtered_data.sort(key=lambda x: x["description"], reverse=True)
+
+
+    if not (len(filtered_data) > 0):
+        print("Не найдено ни одной транзакции, подходящей под ваши условия фильтрации")
+    else:
+        print(f"Всего банковских операций в выборке: {len(filtered_data)}\n")
+        for i in filtered_data:
+            print(f"{i.get('date', False)} {i.get('description', False)}\n{i.get('to', False)}\nСумма: {i.get('amount', False)} {i.get('currency_name', False)}.")
+            print("*******************")
+
+
+
+if __name__ == '__main__':
+    main()
